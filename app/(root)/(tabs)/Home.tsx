c23099aecf7e9323b5d20def7d5495598088f602
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-08-26 21:24:47 +0530

    setup clerk within the project

--- app/(root)/(tabs)/Home.tsx
+++ app/(root)/(tabs)/Home.tsx
@@ -1,12 +1,23 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
-const Home = () => {
+export default function Page() {
+	const { user } = useUser();
+
 	return (
-		<SafeAreaView>
-			<Text>Home</Text>
-		</SafeAreaView>
+		<View>
+			<SignedIn>
+				<Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+			</SignedIn>
+			<SignedOut>
+				<Link href="/(auth)/Signin">
+					<Text>Sign In</Text>
+				</Link>
+				<Link href="/(auth)/Signup">
+					<Text>Sign Up</Text>
+				</Link>
+			</SignedOut>
+		</View>
 	);
-};
-
-export default Home;
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,13 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import { StatusBar } from "react-native";
 import "react-native-reanimated";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 export default function RootLayout() {
 	const [loaded] = useFonts({
@@ -19,6 +21,10 @@ export default function RootLayout() {
 		"Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
 	});
 
+	if (!publishableKey) {
+		throw new Error("Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY");
+	}
+
 	useEffect(() => {
 		if (loaded) {
 			SplashScreen.hideAsync();
@@ -30,14 +36,13 @@ export default function RootLayout() {
 	}
 
 	return (
-		<>
-			<StatusBar barStyle="dark-content" />
+		<ClerkProvider publishableKey={publishableKey}>
 			<Stack>
 				<Stack.Screen name="index" options={{ headerShown: false }} />
 				<Stack.Screen name="(auth)" options={{ headerShown: false }} />
 				<Stack.Screen name="(root)" options={{ headerShown: false }} />
 				<Stack.Screen name="+not-found" />
 			</Stack>
-		</>
+		</ClerkProvider>
 	);
 }
