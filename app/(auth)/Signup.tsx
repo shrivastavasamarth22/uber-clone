d35b78a33a566076452be4ce500d9e6ffe2c609f
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-08-27 14:18:32 +0530

    Added success modal to sign up screen

--- app/(auth)/Signup.tsx
+++ app/(auth)/Signup.tsx
@@ -1,13 +1,17 @@
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View, Alert } from "react-native";
 import InputField from "@/components/InputField";
 import { useState } from "react";
 import { icons, images } from "@/constants";
 import CustomButton from "@/components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import OAuth from "@/components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
+import { ReactNativeModal } from "react-native-modal";
 
 const Signup = () => {
+	const { isLoaded, signUp, setActive } = useSignUp();
+	const [showSuccessModal, setShowSuccessModal] = useState(false);
+
 	const [form, setForm] = useState({
 		name: "",
 		email: "",
@@ -15,12 +19,10 @@ const Signup = () => {
 	});
 
 	const [verification, setVerification] = useState({
-		state: "default",
+		state: "success",
 		error: "",
 		code: "",
 	});
-
-	const { isLoaded, signUp, setActive } = useSignUp();
 
 	const onSignUpPress = async () => {
 		if (!isLoaded) {
@@ -41,6 +43,7 @@ const Signup = () => {
 			});
 		} catch (err: any) {
 			console.error(JSON.stringify(err, null, 2));
+			Alert.alert("Error", err.errors[0].longMessage);
 		}
 	};
 
@@ -81,7 +84,7 @@ const Signup = () => {
 				<View className="relative w-full h-[250px]">
 					<Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
 					<Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-						, Create Your Account
+						Create Your Account
 					</Text>
 				</View>
 				<View className="p-5">
@@ -115,7 +118,6 @@ const Signup = () => {
 						onPress={onSignUpPress}
 					/>
 
-					{/* oAuth */}
 					<OAuth />
 
 					<Link
@@ -126,7 +128,27 @@ const Signup = () => {
 						<Text className="text-primary-500">Log In</Text>
 					</Link>
 				</View>
+
 				{/* Verfication Modal */}
+				<ReactNativeModal isVisible={verification.state === "success"}>
+					<View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+						<Image
+							source={images.check}
+							className="w-[110px] h-[110px] mx-auto my-5"
+						/>
+						<Text className="text-3xl font-JakartaBold text-center">
+							Verified
+						</Text>
+						<Text className="text-base text-gray-400 font-Jakarta text-center mt-2">
+							You have successfully verified your account!
+						</Text>
+						<CustomButton
+							title="Browse Home"
+							onPress={() => router.replace("/(root)/(tabs)/Home")}
+							className="mt-5"
+						/>
+					</View>
+				</ReactNativeModal>
 			</View>
 		</ScrollView>
 	);
