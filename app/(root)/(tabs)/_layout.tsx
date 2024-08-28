eb757f98de123370ac0b1539efe1b7cb7f01e02b
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-08-28 20:40:21 +0530

    styled the bottom nav bar

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,11 +1,30 @@
 import { icons } from "@/constants";
 import { Tabs } from "expo-router";
-import { Image, View } from "react-native";
+import { Image, ImageSourcePropType, View } from "react-native";
 
-const TabIcon = () => (
-	<View>
-		<View>
-			<Image />
+const TabIcon = ({
+	source,
+	focused,
+}: {
+	source: ImageSourcePropType;
+	focused: boolean;
+}) => (
+	<View
+		className={`flex flex-row justify-center items-center rounded-full ${
+			focused ? "bg-general-300" : ""
+		}`}
+	>
+		<View
+			className={`rounded-full w-12 h-12 items-center justify-center ${
+				focused ? "bg-general-400" : ""
+			}`}
+		>
+			<Image
+				source={source}
+				tintColor="white"
+				resizeMode="contain"
+				className="w-7 h-7"
+			/>
 		</View>
 	</View>
 );
@@ -14,6 +33,22 @@ const Layout = () => (
 		initialRouteName="index"
 		screenOptions={{
 			tabBarActiveTintColor: "white",
+			tabBarInactiveTintColor: "white",
+			tabBarShowLabel: false,
+			tabBarStyle: {
+				backgroundColor: "#333",
+				borderRadius: 50,
+				paddingBottom: 0,
+				overflow: "hidden",
+				marginHorizontal: 10,
+				marginBottom: 20,
+				height: 78,
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+				flexDirection: "row",
+				position: "absolute",
+			},
 		}}
 	>
 		<Tabs.Screen
